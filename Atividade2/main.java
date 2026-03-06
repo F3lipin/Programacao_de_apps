f2f353cff9f3b4c279f4e6e130e89b48b399ab24
F3lipin <felipedonascimentosantos468@gmail.com>
2026-03-06 16:10:44 -0300

    Add files via upload

---
+++ Atividade2/main.java
@@ -0,0 +1,11 @@
+package Atividade2;
+public class main {
+    public static void main(String[] args) {
+
+       Carro v1 = new Carro("Chevrolet", 200, 300);
+       
+       System.out.print(v1.getvelocidadeatual());
+
+    }
+    
+}
